5f17e872efee54b150501895e53e819f2e6e036c
a4a881d4 <a4a881d4@163.com>
2013-04-06 16:34:36 +0800

    examples

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
-ldpc:
-	@echo del ldpc.jar if exist
-	rm ldpc.jar -f
+src:=$(wildcard src/com/jove/ldpc/*.java) 
+ldpc.jar:${src}
 	@echo build 
 	javac -s src -d build src/com/jove/ldpc/*.java
 	@echo jar
 	jar cf ldpc.jar -C build .
 
-Ltest:
+build/LDPCTest.class:ldpc.jar test/LDPCTest.java
 	javac -cp ./ldpc.jar -d build test/*.java
 	
+test:build/LDPCTest.class
+	java -cp build:ladpc.jar LDPCTest examples/test.deg examples/code 0.25 28 72 83
 
--- test/LDPCTest.java
+++ test/LDPCTest.java
@@ -1,17 +1,33 @@
 import com.jove.ldpc.*;
 import java.io.*;
+import java.util.Scanner;
 
 class LDPCTest {
 
 	public static void main( String[] argv ) throws IOException{
-		File file = new File(argv[0]);
-		int[] dg = {2,3,4,5,8,10,12};
-		double[] df = {0.4717,0.3336,0.0102,0.0426,0.0070,0.0049,0.1300};  
+		Scanner deg = new Scanner( new File(argv[0]) );
+		File ofile = new File(argv[1]);
+		double rate = Double.parseDouble(argv[2]);
+		int bl = Integer.parseInt(argv[4]);
+		int N = Integer.parseInt(argv[3]);
+		int poly = Integer.parseInt(argv[5],16);
+
+		int length = deg.nextInt();
+		int dg[] = new int[length];
+		double df[] = new double[length];
+		int i;
+		for( i=0;i<length;i++ )
+			dg[i]=deg.nextInt();
+		for( i=0;i<length;i++ )
+			df[i]=deg.nextDouble();
+
+//		int[] dg = {2,3,4,5,8,10,12};
+//		double[] df = {0.4717,0.3336,0.0102,0.0426,0.0070,0.0049,0.1300};  
 		double[] range = {1.0,1.1};
-		Degree deg = new Degree(dg,df);
-		LDPCDBItem item = new LDPCDBItem( deg, "", "", range, 0.25 );
-		PrintWriter writer = new PrintWriter(new FileWriter(file));
-		item.logCode(writer,72,28,0x83);
+		Degree Ddeg = new Degree(dg,df);
+		LDPCDBItem item = new LDPCDBItem( Ddeg, "", "", range, rate );
+		PrintWriter writer = new PrintWriter(new FileWriter(ofile));
+		item.logCode(writer,bl,N,poly);
 		writer.flush();
 		writer.close();
 	}
